4a09d5927f83fc86b4d8c736a837cc417cccaa11
irgi <irgiaditpratama@gmail.com>
2025-10-23 14:59:25 +0700

    module wo 1.0.1

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -5,7 +5,7 @@ import bcrypt from 'bcrypt';
 
 const prisma = new PrismaClient();
 
-export const authOptions: AuthOptions = {
+const authOptions: AuthOptions = {
   providers: [
     CredentialsProvider({
       name: 'Credentials',
